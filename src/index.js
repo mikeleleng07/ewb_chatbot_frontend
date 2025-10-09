31148fecff7e606ca1abc1b1bb6b9024eec56c36
mikeleleng <mbmanzanilla@eastwestbanker.com>
2025-10-09 18:50:28 +0800

    test update

--- src/index.js
+++ src/index.js
@@ -1,19 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
-const root = ReactDOM.createRoot(document.getElementById('webchat'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import React from 'react';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import './index.css';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Polyfill structuredClone
+if (typeof structuredClone === 'undefined') {
+  window.structuredClone = obj => JSON.parse(JSON.stringify(obj));
+}
+
+// Mount React
+const container = document.getElementById('webchat');
+if (!container) console.error('Webchat container not found!');
+else
+  createRoot(container).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
